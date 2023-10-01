fce8c2f3d4b1e2ca6a6988562c26eb39def0d213
nitin-agam <nitinagam17@gmail.com>
2023-10-01 12:28:01 +0530

    Project creation

---
+++ Weather/Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Nitin Aggarwal on 30/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

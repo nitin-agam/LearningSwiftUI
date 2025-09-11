35c4dc81d2ea49d214bf98bbed6dccde70d5a110
Nitin Aggarwal <nitinagam17@gmail.com>
2025-09-11 16:46:16 +0530

    Code refactored in user list view

--- LBTAChat/LBTAChat/New Message/UserListView.swift
+++ LBTAChat/LBTAChat/New Message/UserListView.swift
@@ -23,23 +23,19 @@ struct UserListView: View {
         ScrollView {
             ForEach(viewModel.users) { user in
                 VStack(alignment: .leading) {
-                    HStack {
-                        WebImage(url: URL(string: user.profileImageUrl))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50)
-                            .cornerRadius(50)
-                            .overlay {
-                                Circle()
-                                    .stroke(style: StrokeStyle(lineWidth: 1))
-                            }
-                            .shadow(radius: 5)
-                        
-                        Text(user.email)
-                            .font(.system(size: 17, weight: .semibold))
-                            .foregroundStyle(.primary)
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        HStack {
+                            profileImageView(for: user.profileImageUrl)
+                            
+                            Text(user.email)
+                                .font(.system(size: 17, weight: .semibold))
+                                .foregroundStyle(Color(.label))
+                        }
+                        .padding(.horizontal)
                     }
-                    .padding(.horizontal)
+                    
                     Divider()
                         .padding(.vertical, 6)
                 }
@@ -56,6 +52,20 @@ struct UserListView: View {
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    private func profileImageView(for urlString: String) -> some View {
+        WebImage(url: URL(string: urlString))
+            .resizable()
+            .scaledToFill()
+            .frame(width: 50, height: 50)
+            .cornerRadius(50)
+            .overlay {
+                Circle()
+                    .stroke(style: StrokeStyle(lineWidth: 1))
+            }
+            .shadow(radius: 5)
     }
 }
 
